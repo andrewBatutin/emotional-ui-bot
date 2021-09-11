04e7690b94fbee1fce0a6f68867f6ca6aa7bf1b4
Andrew Batutin <abatutin@gmail.com>
2021-09-11 16:52:02 +0300

    bot setup

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import random
+
 import telebot
 import requests
 from boto_req_endpoint import send_message_for_inference
 
+random.seed(42)
 # {'label': 'LABEL_0', 'score': 0.9968411326408386}
 LABEL_NORMAL = 'LABEL_0'
 token = '1928238940:AAH6GLnnthf1yek4jX71OgH1BEtstQ8ZYmE'
 
-
+img_for_reaction = ['https://i.giphy.com/media/l1J9NRpOeS7i54xnW/giphy.webp',
+                    'https://i.giphy.com/media/5fBH6zodw7VMuR8uUnu/giphy.webp',
+                    'https://i.giphy.com/media/gjpteFyzpzDlS/giphy.webp']
 
 bot = telebot.TeleBot(token)
 
@@ -24,11 +29,13 @@ def phu(message):
         toxic_count += 1
 
     if toxic_count > 0:
-        bot.send_message(message.chat.id, words + str(is_toxic_msg))
-        bot.send_animation(message.chat.id, 'https://i.giphy.com/media/l1J9NRpOeS7i54xnW/giphy.webp')
+
+        img_indx = random.randint(0,len(img_for_reaction))
         toxic_count = 0
-    # bot.send_message(message.chat.id, response.text)
-    # bot.forward_message('558800780', message.chat.id, message.message_id)
+
+        bot.send_message(message.chat.id, '@Wild314HackSuperRes_bot says:')
+        bot.send_animation(message.chat.id, img_for_reaction[img_indx])
+
 
 
 def is_toxic(results):
